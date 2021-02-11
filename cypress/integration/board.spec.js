64331dec387199c09a2ad24ad5541641ebe1d592
NathanThomasWebDeveloper <hellonathanthomas@gmail.com>
2021-02-11 23:00:49 +0100

    Add adapt board to container height and cypress testing

---
+++ cypress/integration/board.spec.js
@@ -0,0 +1,37 @@
+import {host} from "../support";
+
+describe('sizes are correct', () => {
+    beforeEach(() => {
+        cy.visit(host)
+    })
+    it('boards are square', function () {
+        cy.getBySel('component-board').should('be.visible')
+            .then(elements => {
+                for (let i = 0; i < elements.length; i++) {
+                    const height = elements[i].getBoundingClientRect().height
+                    const width = elements[i].getBoundingClientRect().width
+                    expect(height).to.be.equal(width)
+                }
+            })
+    })
+    it('all squares are square', function () {
+        cy.getBySel('board-square').should('be.visible')
+            .then(elements => {
+                for (let i = 0; i < elements.length; i++) {
+                    const height = elements[i].getBoundingClientRect().height
+                    const width = elements[i].getBoundingClientRect().width
+                    expect(height).to.be.equal(width)
+                }
+            })
+    })
+    it('all squares are equal size', function () {
+        cy.getBySel('board-square').should('be.visible')
+            .then(elements => {
+                const height = elements[0].getBoundingClientRect().height
+                for (let i = 0; i < elements.length; i++) {
+                    expect(elements[i].getBoundingClientRect().height).to.be.equal(height)
+                }
+
+            })
+    })
+})
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Board.module.scss'
 import {boardColors} from '../constants';
 import Square from "./Square";
+import {useMeasure} from "react-use";
 
 const Board = () => {
-    return (<div data-test={"component-board"} className={styles.Board}>
+    const [ref, {width, height}] = useMeasure();
+    const [boardSize, setBoardSize] = useState(300);
+    useEffect(() => {
+        setBoardSize((width > height) ? height : width)
+    }, [width, height])
+    // @ts-ignore fixme
+    return (<div ref={ref} data-test={"component-container"} className={styles.Container}>
+        <div data-test={"component-board"}  style={{height: boardSize, width: boardSize}} className={styles.Board}>
         {boardColors.map((color, index) => (<Square key={color + index} color={color}/>))}
-    </div>)
+    </div></div>)
 }
 export default Board
